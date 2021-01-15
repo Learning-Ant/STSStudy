63b5616b5aa9f6f0241e7e2d0f845fce07e9ead9
JAESEONG PARK <a86007681@gmail.com>
2021-01-15 10:30:49 +0900

    Title : complete the 02_bean project
    Content :
    How to create beans of List, Set, Map

--- 02_bean/src/main/java/com/koreait/spring/SpringMainClass.java
+++ 02_bean/src/main/java/com/koreait/spring/SpringMainClass.java
@@ -1,10 +1,23 @@
 package com.koreait.spring;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class SpringMainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String resourceLocation = "classpath:app-context.xml";
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext(resourceLocation);
+		
+		ListHandler handler1 = ctx.getBean("listHandler", ListHandler.class);
+		handler1.listInfo();
+		
+		SetHandler handler2 = ctx.getBean("setHandler", SetHandler.class);
+		handler2.setInfo();
+		
+		MapHandler handler3 = ctx.getBean("mapHandler", MapHandler.class);
+		handler3.mapInfo();
 	}
 
 }
